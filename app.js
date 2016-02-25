30cfe86cf1171cf8e7e327c75eee589be87e0a56
Peter Joseph <peter.joseph@soltius.co.nz>
2016-02-25 17:30:38 +1300

    Added config files for hinters and stuff

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
    res.send('Hello World'); 
 });
 
-app.get('/books', function(req, res) {
+app.get('/books', function (req, res) {
    res.send('Hello Books'); 
 });
 
 app.listen(5000, function(err) {
-    console.log('running server on port ' + port)
+    console.log('running server on port ' + port);
 });
