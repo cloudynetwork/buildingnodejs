ebdf4cd84cf9734f7cc1cadb149345b10951133c
Peter Joseph <peter.joseph@soltius.co.nz>
2016-03-03 08:54:26 +1300

    Added nodemon stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jshint');
 var inject = require('gulp-inject');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
+    'use strict';
     return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
@@ -15,22 +17,42 @@ gulp.task('style', function () {
 });
 
 gulp.task('inject', function () {
-    var wiredep = require('wiredep').stream;
+    'use strict';
 
-    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+    var wiredep = require('wiredep').stream,
 
-    var injectOptions = {
-        ignorePath: '/public'
-    };
+        injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {
+            read: false
+        }),
 
-    var options = {
-        bowerJson: require('./bower.json'),
-        directory: './public/lib',
-        ignorePath: '../../public'
-    };
+        injectOptions = {
+            ignorePath: '/public'
+        },
+
+        options = {
+            bowerJson: require('./bower.json'),
+            directory: './public/lib',
+            ignorePath: '../../public'
+        };
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    'use strict';
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting...');
+        });
 });
