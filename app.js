007f1658f0b248d54e62833d93c34df9820c4300
Peter Joseph <peter.joseph@soltius.co.nz>
2016-03-10 17:28:54 +1300

    added EJS capability insted Handlebar

--- app.js
+++ app.js
@@ -7,22 +7,11 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-<<<<<<< Updated upstream
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-
-app.set('view engine', '.hbs');
-
-app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render',
-        list: ['a', 'b', 'c']
-=======
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('index', {title: 'Hello from render',
         list: ['a', 'b', 'c', 'd']
->>>>>>> Stashed changes
     });
 });
 
