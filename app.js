3a17f4814a00f5c651cd80ecc3ab99d308c5a9f7
Peter Joseph <peter.joseph@soltius.co.nz>
2016-03-10 17:25:04 +1300

    Additional handle bars functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
+<<<<<<< Updated upstream
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 
@@ -15,6 +16,13 @@ app.set('view engine', '.hbs');
 app.get('/', function (req, res) {
     res.render('index', {title: 'Hello from render',
         list: ['a', 'b', 'c']
+=======
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index', {title: 'Hello from render',
+        list: ['a', 'b', 'c', 'd']
+>>>>>>> Stashed changes
     });
 });
 
