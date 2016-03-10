7412794bd70af1948ba6e74cc16100c246319754
Peter Joseph <peter.joseph@soltius.co.nz>
2016-03-10 17:08:21 +1300

    Finshed test handlebar implementation

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-var handlebars = require('express-handlebars')
+var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    res.render('index', {
+    res.render('index', {title: 'Hello from render',
         list: ['a', 'b', 'c']
     });
 });
